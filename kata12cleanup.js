26a4b5643c11df3202e3cb5dfc1e502d6011d705
Sahil Athia <sahilathia918@gmail.com>
2020-10-29 23:50:14 +0000

    cleaning up kata-12 stretch

---
+++ kata12cleanup.js
@@ -0,0 +1,25 @@
+const organizeInstructors = function(instructors) {
+  let final = {};
+  instructors.forEach(element => {
+    if (final[element.course]) {
+      final[element.course].push(element.name);
+    } else {
+      final[element.course] = [];
+      final[element.course].push(element.name);
+    }
+  });
+  return final;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
